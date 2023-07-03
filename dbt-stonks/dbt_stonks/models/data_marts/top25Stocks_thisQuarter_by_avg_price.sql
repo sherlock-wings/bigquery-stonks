a8bedeccd83cd5f1e116a32af6f10ac8dbeee656
Patrick Callahan <patrick.f.callahan@gmail.com>
2023-07-03 16:30:21 -0400

    added models to get top 25 most valuable stocks by avg value over the current year, current quarter, current month, current week, and current day

---
+++ dbt-stonks/dbt_stonks/models/data_marts/top25Stocks_thisQuarter_by_avg_price.sql
@@ -0,0 +1,17 @@
+with tbl as 
+(select extract(year from max(call_at)) as year,
+        extract(quarter from max(call_at)) as quarter,
+        ticker_symbol,
+        stock_name,
+        industry,
+        round(avg(price), 2) as avg_value_USD
+ from {{source('realStonks_api_data', 'stg_sp500')}} 
+ where extract(quarter from call_at) = extract(quarter from current_date()) 
+ group by ticker_symbol, stock_name, industry
+ order by avg_value_USD desc
+ limit 25)
+select dense_rank() over(order by tbl.avg_value_USD desc)
+       as stock_value_rank,
+       tbl.* 
+from tbl
+order by stock_value_rank 
